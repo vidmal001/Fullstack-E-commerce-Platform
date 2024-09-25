b4ba7cdce2d0376b3375b57d0cb23a57c21b574f
vidmal001 <156089675+vidmal001@users.noreply.github.com>
2024-09-25 14:18:36 +0530

    Create auth.route.js

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { login, logout, signup , refreshToken , getProfile } from "../controllers/auth.controller.js";
+
+const router = express.Router(); // create express router
+
+router.post("/signup", signup); // http://localhost:5000/api/auth/signup
+
+router.post("/login", login); // http://localhost:5000/api/auth/login
+
+router.post("/logout", logout); // http://localhost:5000/api/auth/logout
+
+router.post("/refresh-token",refreshToken) // http://localhost:5000/api/auth/refresh-token
+
+router.get("/profile",getProfile) // http://localhost:5000/api/auth/profile
+export default router;
