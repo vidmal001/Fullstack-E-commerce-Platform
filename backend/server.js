0b6dd93b43d851ebdf3053d42d15a5c988787c39
vidmal001 <156089675+vidmal001@users.noreply.github.com>
2024-10-07 23:27:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 // routes
 import authRoutes from "./routes/auth.route.js"; // when you import something from a local file you need to use .js
+import productRoutes from "./routes/product.route.js";
 // database
 import { connectDB } from "./config/db.js";
 
@@ -15,6 +16,7 @@ app.use(express.json()); // this allows you to parse JSON data from the request 
 app.use(cookieParser()); // this allows you to parse cookie data from an HTTP request
 
 app.use("/api/auth", authRoutes); // use auth route
+app.use("/api/products",productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
