405a46e74fc020f29be0a54294b4344ce416a23e
vidmal001 <156089675+vidmal001@users.noreply.github.com>
2024-10-08 16:11:15 +0530

    Create cart.route.js

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { addToCard, removeAllFromCart,updateQuantity,getCartProducts } from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/",protectRoute, getCartProducts);
+router.post("/", protectRoute,addToCard);
+router.delete("/",protectRoute,removeAllFromCart);
+router.put("/:id",protectRoute,updateQuantity);
+
+export default router;
