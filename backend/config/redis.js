6cfa74c0f49e6cb029c06279ff5be22479ced6f4
vidmal001 <156089675+vidmal001@users.noreply.github.com>
2024-10-10 13:53:51 +0530

    Update redis.js

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -7,4 +7,4 @@ export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
 
 // Redis is essentially a key-value store, and we can think of it as a giant JSON object. It supports various data structures, such as hashes, sets, lists, and sorted sets. For this project, we will specifically be using strings or sets.
 
-await redis.set('foo', 'bar');
+// await redis.set('foo', 'bar');
