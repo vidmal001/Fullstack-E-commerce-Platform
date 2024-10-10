cf4302d2b50c7a5e4f7aecb99e63d8f1d7e0d9ab
vidmal001 <156089675+vidmal001@users.noreply.github.com>
2024-10-10 13:54:51 +0530

    Create useUserStore.js

---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,71 @@
+import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useUserStore = create((set, get) => ({
+  user: null,
+  loading: false,
+  checkingAuth: true,
+
+  signup: async ({ name, email, password, confirmPassword }) => {
+    set({ loading: true });
+
+    if (password.length < 6) {
+      set({ loading: false });
+      return toast.error("Password should be at least 6 characters long");
+    }
+
+    if (password !== confirmPassword) {
+      set({ loading: false });
+      return toast.error("Passwords do not match");
+    }
+
+    try {
+      const res = await axios.post("/auth/signup", { name, email, password });
+      set({ user: res.data, loading: false });
+      toast.success("Account created successfully");
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response.data.message || "An error occurred");
+    }
+  },
+
+  login: async (email, password) => {
+    set({ loading: true });
+
+    try {
+      const res = await axios.post("/auth/login", { email, password });
+
+      set({ user: res.data, loading: false });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      set({ loading: false });
+      toast.error(error.response.data.message || "An error occurred");
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axios.post("/auth/logout");
+      set({ user: null });
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message || "An error occurred during logout"
+      );
+    }
+  },
+
+  checkAuth: async () => {
+    set({ checkingAuth: true });
+    try {
+      // return user profile to us
+      const response = await axios.get("/auth/profile");
+      // once we got the response back state gonna be false
+      set({ user: response.data, checkingAuth: false });
+    } catch (error) {
+      console.log(error.message);
+      set({ checkingAuth: false, user: null });
+      // In both success and failure cases, the app has finished the authentication check, so checkingAuth should be set to false.
+    }
+  },
+}));
