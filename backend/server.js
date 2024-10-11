84aad86e197c6af160a64fcdf11359bb7c7fabb5
vidmal001 <156089675+vidmal001@users.noreply.github.com>
2024-10-11 16:34:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config(); // load environment variables from .env
 const app = express(); // create express app
 const PORT = process.env.PORT || 5000; // get port from environment variable or use 5000 as default
 
-app.use(express.json()); // this allows you to parse JSON data from the request body of an HTTP request
+app.use(express.json({ limit: "10mb" })); // this allows you to parse JSON data from the request body of an HTTP request
 app.use(cookieParser()); // this allows you to parse cookie data from an HTTP request
 
 app.use("/api/auth", authRoutes); // use auth route
